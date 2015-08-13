3de21ffb5c3ee8220dca3fdb815c19ce332ed5ac
Chris Thoung <chris.thoung@gmail.com>
2015-08-13 23:24:08 +0100

    Update CHANGELOG, README and package init file

--- FSICdata/__init__.py
+++ FSICdata/__init__.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+FSICdata
+========
+FSICdata is a Python package to provide tools to convert raw economic data to
+common data formats. It provides data-processing tools to support macroeconomic
+modelling using FSIC.
+
+"""
 
 
 from FSICdata.version import MAJOR, MINOR, PATCH, VERSION as __version__
